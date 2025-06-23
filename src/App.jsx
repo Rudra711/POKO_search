fa90a52ec24ebe276a7c05726a5238749a372cff
Rudra711 <125131041+Rudra711@users.noreply.github.com>
2025-06-23 18:26:16 +0530

    make registration working

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ function App() {
           ) : (
             <>
               <Route path="/login" element={<Login />} />
+              <Route path="/registration" element={<Registration />} /> 
               <Route path="*" element={<Navigate to="/login" replace />} />
             </>
           )}
